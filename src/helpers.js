a09f9f7db3df93dd0f1093da41ca950e153c7454
anderson.santos <anderson.santos@pcinformatica.com.br>
2016-06-24 19:06:03 -0300

    bugfix

--- src/helpers.js
+++ src/helpers.js
@@ -3,6 +3,33 @@
  */
 rz.helpers.StringBuilder = StringBuilder;
 rz.helpers.generateRandomID = generateRandomID;
-rz.helpers.ensureFunction = function(f){
-    return (f===undefined) ? function(){}:f;
+rz.helpers.ensureFunction = function (f) {
+    return (f === undefined) ? function () {
+    } : f;
+};
+rz.helpers.jsonUtils = {
+    getDataAtPath: function (obj, path) {
+        var parts = path.split('.');
+        var val = obj;
+        parts.every(function (part) {
+            val = val[part];
+            return val !== undefined;
+        });
+        return val;
+    },
+    setDataAtPath: function(obj,path,value){
+        var parts = path.split(".");
+        var last = obj;
+        parts.forEach(function (it, ix) {
+            if ((ix == parts.length - 1)) {
+                last[it] = value;
+            }
+            else {
+                if (last[it] === undefined) {
+                    last[it] = {};
+                }
+            }
+            last = last[it];
+        });
+    }
 };
