b9c8747481d6b9a8f8822823dea47f4249bf0d8f
Anderson Norberto Santos <anderson.santos@pcinformatica.com.br>
2017-03-31 16:20:05 -0300

    Minor adjusts on uri utils to support hash params

--- src/rz.utils.uri.js
+++ src/rz.utils.uri.js
@@ -3,30 +3,46 @@
  */
 rz.utils.uri = {
     getSearch : function(url){
-        var fploc = url.indexOf("?");
-        return (fploc==-1) ? "" : url.substring(fploc);
+
+        var fploc_i = url.indexOf("?");
+        var fploc_h = url.indexOf("#");
+        var fploc_d = url.indexOf("&");
+        var foundValues = [];
+        if(fploc_i >= 0) foundValues.push(fploc_i);
+        if(fploc_h >= 0) foundValues.push(fploc_h);
+        if(fploc_d >= 0) foundValues.push(fploc_d);
+        if(foundValues.length==0){
+            return "";
+        }
+        else{
+            var min = foundValues.reduce(function(a, b) {
+                return Math.min(a, b);
+            });
+            return url.substring(min);
+        }
+        //return (fploc==-1) ? "" : url.substring(fploc);
     },
     getParamCount : function(url){
         var query = this.getSearch(url);
-        return (query.match(new RegExp(/[\?|&][a-zA-Z0-9+]+=([^&*]*)/g))||[]).length;
+        return (query.match(new RegExp(/[\?|&|#][a-zA-Z0-9+]+(=([^&|#|?*]*))?/g))||[]).length;
     },
     getParamList : function(url){
         var query = this.getSearch(url);
-        var rawParams = query.match(new RegExp(/([\?|&][a-zA-Z0-9+]+=)/g))||[];
+        var rawParams = query.match(new RegExp(/([\?|&|#][a-zA-Z0-9+]+=?)/g))||[];
         var oparams = [];
         rawParams.forEach(function(it){
-            oparams.push(it.replace(/[?|&|=]/g,""));
+            oparams.push(it.replace(/[?|&|=|#]/g,""));
         });
         return oparams;
     },
     hasParam : function(url,p){
         var query = this.getSearch(url);
-        return (query.match(new RegExp('[?&]' + p + '=([^&]+)')) || [])[0] !==undefined;
+        return (query.match(new RegExp('[?&#]' + p + '=([^&]+)')) || [])[0] !==undefined;
     },
     getParamValue : function(url,p){
         if(this.hasParam(url,p)){
             var query = this.getSearch(url);
-            return ((query.match(new RegExp('[?&]' + p + '=([^&]+)')) || [])[0]||"").replace(/[?|&][a-zA-Z0-9*]+=/g,"");
+            return ((query.match(new RegExp('[?&#]' + p + '=([^&|^?|^#]+)')) || [])[0]||"").replace(/[?|&|#][a-zA-Z0-9*]+=/g,"");
         }
         else{
             return undefined;
